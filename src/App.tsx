d29006e7817f3febe608170d1cbd2cb45d2d14a8
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-06-23 22:43:58 +0530

    feat: redesign  (#78)
    Signed-off-by: Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,85 @@
-import React from "react"
+import Me from './me.jpg';
 
-interface LinkProps {
-  href: string,
-  children: string | React.JSX.Element | React.JSX.Element[],
+const timeline = [
+  {
+    title: 'Certfied Ethical Hacker v13',
+    description: 'EC Council (May 2025 - valid till May 2028)',
+  },
+  {
+    title: 'Penetration Tester',
+    description: 'Acquisory Cyber Consulting LLP (April 2024 - Present)',
+  },
+  {
+    title: 'Bachelor of Science',
+    description: 'University of Toronto (graduated June 2024)',
+  },
+  {
+    title: 'Software Developer',
+    description: 'Constant Health (June 2021 - September 2022)',
+  },
+];
+
+interface TimelineProps {
+  details: {
+    title: string;
+    description: string;
+  }[];
 }
 
-function Link({
-  href,
-  children,
-}: LinkProps): React.JSX.Element {
+function Timeline({
+  details
+}: TimelineProps) {
   return (
-    <a href={href} rel="noopener noreferrer" target="_blank">{children}</a>
+    <div className="relative border-l-4 border-gray-300 ml-4 pl-6 space-y-12 ml-[100px]">
+      {details.map((item, idx) => (
+        <div key={idx}>
+          <div style={{ top: `${idx * 92}px` }} className="absolute -left-[9px] w-4 h-4 bg-black rounded-full border-2 border-white shadow"></div>
+          <h3 className="text-lg font-semibold text-black">{item.title}</h3>
+          <p className="text-sm text-gray-600">{item.description}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
 function App() {
 
   return (
-    <div style={{ margin: 10 }}>
-      <h1 data-testid='about'>AYUSHMAN CHHABRA</h1>
-      <section style={{ paddingTop: 20 }}>
-        <h3>Me in 10 seconds</h3><br />
-        <p>Economics student turned software developer turned security analyst.</p>
-        <p>Interested in literature, music and fitness.</p>
-        <p>Occassionally touches grass.</p>
+    <div>
+      <nav>
+        <ul className="h-[50px] w-full flex items-center justify-end gap-10 pt-[30px] pr-[80px]">
+          <li>
+            <a className="text-black font-semibold hover:text-gray-500 transition-colors duration-200 delay-100" href="/home">Work</a>
+          </li>
+          <li>
+            <a className="text-black font-semibold hover:text-gray-500 transition-colors duration-200 delay-100" href="https://github.com/ayushmanchhabra" rel="noopener noreferrer" target="_blank">Projects</a>
+          </li>
+          <li>
+            <a className="text-black font-semibold hover:text-gray-500 transition-colors duration-200 delay-100" href="https://www.linkedin.com/in/ayushmanchhabra/" rel="noopener noreferrer" target="_blank">Resume</a>
+          </li>
+        </ul>
+      </nav>
+      <section className="h-full pl-20 pt-15 grid grid-cols-[2fr_1fr] items-center flex justify-start items-center">
+        <div className="flex flex-col items-start justify-center">
+          <span className="text-4xl font-semibold text-gray-800">
+            Economics student <br /> turned software developer <br /> turned penetration tester.
+          </span>
+          <br /><br />
+          <span>
+            Interested in old movies, vehicle number plates and good food.
+          </span>
+        </div>
+
+        <div className="max-sm:hidden h-full w-full flex flex-col items-center justify-center pr-[80px]">
+          <img className="border-2" src={Me} alt="Me" />
+        </div>
+
       </section>
 
-      <section style={{ paddingTop: 20 }}>
-        <h3 data-testid='projects'>Projects</h3><br />
-        <p><Link href="https://asyncawake.studio">Async Awake</Link>: A technology blog about coding, software engineering, cyber security and more.</p>
-        <p><Link href="https://github.com/ayushmanchhabra/vsx">vsx</Link>: A library to write vanilla JavaScript applications using JSX syntax without virtual DOM or Proxy magic.</p>
-        <p><Link href="https://github.com/ayushmanchhabra/surf">Surf</Link>: A browser with the goal of inversion of control.</p>
-      </section>
+      <br /><br />
 
-      <section style={{ paddingTop: 20 }}>
-        <h3 data-testid='books'>Books</h3><br />
-        <p><Link href="https://www.goodreads.com/book/show/40121378-atomic-habits">Atomic Habits</Link>: Changed the way I function at Link fundamental level.</p>
-        <p><Link href="https://goodreads.com/book/show/34536488-principles">Principles</Link>: Taught me how to think clearly.</p>
-        <p><Link href="https://www.goodreads.com/book/show/23848190-extreme-ownership">Extreme Ownership</Link>: Taught me why and how to take ownership of tasks.</p>
-      </section>
-
-      <section style={{ paddingTop: 20 }}>
-        <h3 data-testid='videos'>Videos</h3><br />
-        <p><Link href="https://www.youtube.com/watch?v=PUv66718DII">Inventing on Principle | Bret Victor</Link></p>
-        <p><Link href="https://www.youtube.com/watch?v=zKd_kxcxGbo&t=256s">Mediocrity is Rising | Harkirat Singh</Link></p>
-      </section>
-
-      <section style={{ paddingTop: 20 }}>
-        <h3 data-testid='blogs'>Blogs</h3><br />
-        <p><Link href="https://sive.rs/">Derek Sivers</Link></p>
-        <p><Link href="https://patrickcollison.com/">Patrick Colinson</Link></p>
-        <p><Link href="https://paulgraham.com/">Paul Graham</Link></p>
+      <section>
+        <Timeline details={timeline} />
       </section>
     </div>
   )
